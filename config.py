abf491f1d37ccb8663b3e3e7b61e7b6358e3c7fe
yuvi9940814-byte <yuvi9940814@gmail.com>
2026-09-04 11:49:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 DATA_DIR = os.getenv("DATA_DIR", "/app/data")
 os.makedirs(DATA_DIR, exist_ok=True)
 
-API_ID = int(os.getenv("API_ID", "123456"))          # Railway Variables me daalo
+API_ID = int(os.getenv("API_ID", "123456"))
 API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "123456789").split(",") if x.strip()]
+OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "8076739291,8880226666").split(",") if x.strip()]
 
 SESSIONS_DIR = os.path.join(DATA_DIR, "sessions")
 ACCOUNTS_FILE = os.path.join(DATA_DIR, "accounts.json")
